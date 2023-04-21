aec2b98666cc8bf8d5d191c300fe29ed80e33361
Phil Wang <lucidrains@gmail.com>
2023-04-21 10:20:21 -0700

    all we need

--- naturalspeech2_pytorch/naturalspeech2_pytorch.py
+++ naturalspeech2_pytorch/naturalspeech2_pytorch.py
@@ -9,6 +9,42 @@ from einops import rearrange
 def exists(val):
     return val is not None
 
+# attention
+
+class Attention(nn.Module):
+    def __init__(
+        self,
+        dim,
+        *,
+        dim_head = 64,
+        heads = 8
+    ):
+        super().__init__()
+        self.scale = dim_head ** -0.5
+        self.heads = heads
+
+        dim_inner = dim_head * heads
+
+        self.to_qkv = nn.Linear(dim, dim_inner * 3, bias = False)
+        self.to_out = nn.Linear(dim_inner, dim, bias = False)
+
+    def forward(self, x):
+        h = self.heads
+
+        q, k, v = self.to_qkv(x).chunk(3, dim = -1)
+        q, k, v = map(lambda t: rearrange(t, 'b n (h d) -> b h n d', h = h), (q, k, v))
+
+        q = q * self.scale
+
+        sim = einsum('b h i d, b h j d -> b h i j', q, k)
+
+        attn = sim.softmax(dim = -1)
+
+        out = einsum('b h i j, b h j d -> b h i d', attn, v)
+
+        out = rearrange(out, 'b h n d -> b n (h d)')
+        return self.to_out(out)
+
 # classes
 
 class NaturalSpeech2(nn.Module):
