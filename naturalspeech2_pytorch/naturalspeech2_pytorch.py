4131aa459fc3a35b93431fc834a7fc3ee156abb9
Phil Wang <lucidrains@gmail.com>
2023-06-30 09:59:33 -0700

    some cleanup

--- naturalspeech2_pytorch/naturalspeech2_pytorch.py
+++ naturalspeech2_pytorch/naturalspeech2_pytorch.py
@@ -110,8 +110,6 @@ def compute_pitch(spec, sample_rate, hop_length, pitch_fmax=640.0):
     f0 = pw.stonemask(spec.astype(np.double), f0, t, sample_rate)
     return f0
 
-#compute Alignement
-
 # peripheral models
 
 # phoneme - pitch - speech prompt - duration predictors
@@ -300,6 +298,62 @@ def ConvBlock(dim, dim_out, kernel, dropout = 0.):
         Rearrange('b c n -> b n c'),
     )
 
+class DurationPitchPredictorTrunk(nn.Module):
+    def __init__(
+        self,
+        dim = 512,
+        depth = 10,
+        kernel_size = 3,
+        dim_context = None,
+        heads = 8,
+        dim_head = 64,
+        dropout = 0.2,
+        use_resnet_block = True,
+        num_convs_per_resnet_block = 2,
+        num_convolutions_per_block = 3,
+        use_flash_attn = False,
+    ):
+        super().__init__()
+        self.layers = nn.ModuleList([])
+
+        conv_klass = ConvBlock if not use_resnet_block else partial(ResnetBlock, num_convs = num_convs_per_resnet_block)
+
+        for _ in range(depth):
+            layer = nn.ModuleList([
+                nn.Sequential(*[
+                    conv_klass(dim, dim, kernel_size) for _ in range(num_convolutions_per_block)
+                ]),
+                RMSNorm(dim),
+                Attention(
+                    dim,
+                    dim_context = dim_context,
+                    heads = heads,
+                    dim_head = dim_head,
+                    dropout = dropout,
+                    use_flash = use_flash_attn,
+                    cross_attn_include_queries = True
+                )
+            ])
+
+            self.layers.append(layer)
+
+        self.to_pred = nn.Sequential(
+            nn.Linear(dim, 1),
+            Rearrange('... 1 -> ...'),
+            nn.ReLU()
+        )
+    def forward(
+        self,
+        x,
+        encoded_prompts,
+        prompt_mask = None,
+    ):
+        for conv, norm, attn in self.layers:
+            x = conv(x)
+            x = attn(norm(x), encoded_prompts, mask = prompt_mask) + x
+
+        return self.to_pred(x)
+
 class DurationPitchPredictor(nn.Module):
     def __init__(
         self,
@@ -327,35 +381,18 @@ class DurationPitchPredictor(nn.Module):
 
         self.phoneme_token_emb = nn.Embedding(num_phoneme_tokens, dim) if exists(num_phoneme_tokens) else nn.Identity()
 
-        self.pitch_predictor_layers = nn.ModuleList([])
-        self.duration_predictor_layers = nn.ModuleList([])
-
-        conv_klass = ConvBlock if not use_resnet_block else partial(ResnetBlock, num_convs = num_convs_per_resnet_block)
-
-        for _ in range(depth):
-            layer = nn.ModuleList([
-                nn.Sequential(*[
-                    conv_klass(dim_hidden, dim_hidden, kernel_size) for _ in range(num_convolutions_per_block)
-                ]),
-                RMSNorm(dim),
-                Attention(
-                    dim_hidden,
-                    dim_context = dim_encoded_prompts,
-                    heads = heads,
-                    dim_head = dim_head,
-                    dropout = dropout,
-                    use_flash = use_flash_attn,
-                    cross_attn_include_queries = True
-                )
-            ])
-
-            self.pitch_predictor_layers.append(layer)
-            self.duration_predictor_layers.append(copy.deepcopy(layer))
-
-        self.to_pitch_pred = nn.Sequential(
-            nn.Linear(dim_hidden, 1),
-            Rearrange('... 1 -> ...'),
-            nn.ReLU()
+        self.to_pitch_pred = DurationPitchPredictorTrunk(
+            dim = dim_hidden,
+            depth = depth,
+            kernel_size = kernel_size,
+            dim_context = dim_encoded_prompts,
+            heads = heads,
+            dim_head = dim_head,
+            dropout = dropout,
+            use_resnet_block = use_resnet_block,
+            num_convs_per_resnet_block = num_convs_per_resnet_block,
+            num_convolutions_per_block = num_convolutions_per_block,
+            use_flash_attn = use_flash_attn,
         )
 
         self.to_duration_pred = copy.deepcopy(self.to_pitch_pred)
@@ -375,21 +412,7 @@ class DurationPitchPredictor(nn.Module):
 
         x = self.phoneme_token_emb(x)
 
-        # d for duration path
-        # p for pitch path
-
-        d = x.clone()
-        p = x.clone()
-
-        for conv, norm, attn in self.pitch_predictor_layers:
-            p = conv(p)
-            p = attn(norm(p), encoded_prompts, mask = prompt_mask) + p
-
-        for conv, norm, attn in self.duration_predictor_layers:
-            d = conv(d)
-            d = attn(norm(d), encoded_prompts, mask = prompt_mask) + d
-
-        duration_pred, pitch_pred = self.to_duration_pred(d), self.to_pitch_pred(p)
+        duration_pred, pitch_pred = map(lambda fn: fn(x, encoded_prompts = encoded_prompts, prompt_mask = prompt_mask), (self.to_duration_pred, self.to_pitch_pred))
 
         duration_return = F.l1_loss(duration, duration_pred) if exists(duration) else duration_pred
         pitch_return = F.l1_loss(pitch, pitch_pred) if exists(pitch) else pitch_pred
